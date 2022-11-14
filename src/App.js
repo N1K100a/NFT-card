883c2d8322c34e96de2cd51ecb1825377e6c8605
Nika <Chiqovani.Nika01@Gmail.Com>
2022-11-14 17:17:47 +0400

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import mainPhoto from "./images/imageequilibrium.jpg";
-import centerEye from "./images/icon-view.svg"
+import centerEye from "./images/icon-view.svg";
+import styles from "./app.module.css";
+import ethImg from "./images/icon-ethereum.svg";
+import clock from "./images/icon-clock.svg";
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <ImageCon />
       <h3 className='img-Title'><a href="#"> Equilibrium #3429</a></h3>
       <p className='p-Text'>Our Equilibrium collection promotes balance and calm.</p>
+      <ETHAndDay />
+      <User />
       
     </div>
   )
@@ -20,6 +25,27 @@ function ImageCon() {
   </div>)
 
 }
+
+function ETHAndDay() {
+  return (
+    <div className={styles.ethCom}>
+      <p className={styles.p}><img className={styles.eth} src={ethImg} alt="1" /> 0.041 ETH</p>
+      <p className={styles.p2}><img className={styles.eth} src={clock} alt="2" /> 3 days left</p>
+    </div>
+  )
+
+}
+
+function User() {
+  return (
+    <div className='user-Con'>
+      <div className='user-img'></div>
+      <p>Creation of <a href="#">Jules Wyvern</a></p>
+
+    </div>
+  )
+}
+
 
 
 export default App;
